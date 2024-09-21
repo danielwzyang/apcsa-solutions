0697e01ebd525cec34bd36fe5795c4976e1ec59b
Daniel Yang <danielwzyang@gmail.com>
2024-09-21 15:40:00 -0400

    links; uploading to test on github pages

--- build.py
+++ build.py
@@ -5,16 +5,19 @@
 import os
 
 # lists all the files in the directory ./src
-files = os.listdir("./src")
+files = os.listdir("./java")
 
 # opens a template of an html file that i made since we're using it later when we make the new .html files
 with open("./template.html", "r") as file:
     template = file.read()
 
+# i'm using this later on to add a list of all the links into the homepage
+nameList = []
+
 # goes through every file in the ./src directory
 for path in files:
     # opens the java file by going to its location
-    with open("./src/" + path, "r") as file:
+    with open("./java/" + path, "r") as file:
         # reads the file and saves the text
         # strip() just removes empty spaces in the beginning and the end
         code = file.read().strip()
@@ -22,6 +25,9 @@ for path in files:
         # gets the name of the file and removes the .java ending so it becomes just the name
         # ex: Test.java -> Test
         name = path.replace(".java", "")
+
+        # adds the name to the list
+        nameList.append(name)
 
         # now we're going to use the template file and insert our code
         content = template
@@ -33,3 +39,21 @@ for path in files:
         with open("./solutions/" + name + ".html", "w+") as newFile:
             # puts the content into the file
             newFile.write(content)
+
+# string to store all the <li> elements
+elements = ""
+
+# converts the names into <li> elements for html
+for name in nameList:
+    elements += f'<li><a href="./solutions/{name}.html">{name}</a></li>'
+
+# using a template again
+with open("./indexTemplate.html", "r") as file:
+    # inserting our list
+    content = file.read()
+    content = content.replace("[list]", elements)
+    
+    # writing the content
+    with open("./index.html", "w") as file:
+        file.write(content)
+
