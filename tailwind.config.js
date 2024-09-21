1af3711bca402830360805aa51950669e6995421
Daniel Yang <danielwzyang@gmail.com>
2024-09-21 14:56:43 -0400

    updated home page and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./solutions/**/*.{html,js}",
     "./**/*.{html,js}"
   ],
   theme: {
+    fontFamily: {
+        sans: ['"DM Sans"', ...defaultTheme.fontFamily.sans]
+    },
     extend: {},
   },
   plugins: [],
