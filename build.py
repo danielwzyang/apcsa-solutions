b7a3df867fa4410aefcfbc75dc76b02b06ef9209
Daniel Yang <danielwzyang@gmail.com>
2024-09-21 16:09:17 -0400

    fixing solution href

--- build.py
+++ build.py
@@ -46,7 +46,7 @@ elements = ""
 
 # converts the names into <li> elements for html
 for name in nameList:
-    elements += f'<li><a href="apcsa-solutions/solutions/{name}.html">{name}</a></li>'
+    elements += f'<li><a href="solutions/{name}.html">{name}</a></li>'
 
 # using a template again
 with open("./indexTemplate.html", "r") as file:
